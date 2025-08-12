3614ed01921763e6ac929c0be60cf77e1acf7e96
HexEnsemble <hexensemble@hotbakedgoods.com>
2025-08-12 21:02:57 +0100

    Added build script

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+TARGETS=(
+  x86_64-apple-darwin
+  aarch64-apple-darwin
+)
+
+for target in "${TARGETS[@]}"; do
+  echo "🔨 Building for $target"
+  cargo build --release --target $target
+done
+
+echo "🔨 Building for x86_64-pc-windows-gnu"
+cargo zigbuild --release --target x86_64-pc-windows-gnu
+
+echo "✅ All builds done!"
